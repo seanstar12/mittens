b89d3eabb80dd97f54c71fc7a18309c6987642d8
Sean Starnes <seanstar12@gmail.com>
2013-07-28 14:34:05 -0500

    Released providers into seperate file. Updated app.js to support changes

--- server/app.js
+++ server/app.js
@@ -1,34 +1,25 @@
 var http = require('http'),
     io = require('socket.io').listen(8080),
-    config = require('./config');
+    config = require('./config'),
+    providers = require('./providers');
+
+providers.SickBeard.config = config.SickBeard;
 
 io.sockets.on('connection', function (socket) {
   socket.emit('news', { hello: 'world' });
   socket.on('search', function (data) {
     console.log('Search for ' + data.search);
 
-    SickBeard.search(data.search, socket);
+    var sB = providers.SickBeard.search(data.search, socket);
+    runQuery(sB);
   });
 });
 
-var SickBeard = {
-  title: "Sick-Beard",
-  alias: "Tv Shows",
-  host: config.sickbeard.host,
-  port: config.sickbeard.port,
-  api: config.sickbeard.api,
-  search: function(query, socket) {
-    var path = '/api/' + this.api + '/?cmd=sb.searchtvdb&lang=en&name=' + query;
-    runQuery(path, this, socket);
-  },
-}
-
-function runQuery(path, obj, socket) {
-
+function runQuery(obj) {
     var options = {
-      host: obj.host,
-      port: obj.port,
-      path: path,
+      host: obj.data.config.host,
+      port: obj.data.config.port,
+      path: obj.path
     };
     
     callback = function(response) {
@@ -41,7 +32,7 @@ function runQuery(path, obj, socket) {
     
       //the whole response has been recieved, so we just print it out here
       response.on('end', function () {
-        socket.emit('search', str);
+        obj.socket.emit('search', str);
       });
     }
 
